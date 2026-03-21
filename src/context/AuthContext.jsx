c4eab365458192f9e2c861da1a4e40e7d849dc2d
FEDE <federico.pierdominici@gmail.com>
2026-03-21 14:57:53 -0300

    add: termino authContext y agrego la arquitectura

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,3 +11,32 @@ const decodeJWT = (token) => {
     return null;
   }
 };
+
+const AuthProvider = ({ children }) => {
+  const savedToken = sessionStorage.getItem("token")
+  const [token, setToken] = useState(savedToken || null)
+  // 1 - ✅ si tengo token tengo usuario
+  // 2 - descifrar el payload del token
+  const [user, setUser] = useState(() => savedToken ? decodeJWT(savedToken) : null)
+
+  const login = (token) => {
+    sessionStorage.setItem("token",token)
+    setToken(token)
+    setUser(decodeJWT(token))
+  }
+  const logout =(token) => {
+    sessionStorage.removeItem("token")
+    setToken(null)
+    setUser(decodeJWT(null))
+  }
+  
+  return (
+    <AuthContext.Provider value={{ token, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+  }
+  
+const useAuth = () => useContext(AuthContext)
+
+export { AuthProvider, useAuth }
