c33b975db14c6b182d54e36618f3bc745f88b668
FEDE <federico.pierdominici@gmail.com>
2026-03-24 12:11:01 -0300

    arreglo css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    
   )
 }
 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+const AboutUs= () => {
+    return (
+        <Layout>
+            <div className="page">Sobre la página</div>
+
+
+        </Layout>
+
+    )
+}
