dcbcb536aee535514730a5c528cdc05951c7e4ea
FEDE <federico.pierdominici@gmail.com>
2026-04-01 18:32:55 -0300

    add:agrego el AddProduct

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,8 +4,12 @@ const AboutUs= () => {
     return (
         <Layout>
             <div className="page">Sobre la página</div>
-
-
+            <section className="page-section">
+             <h2>Proyecto ecommerce</h2>
+             <p>
+         Proyecto ecommerce FULL STACK con react, node, integración de APIS y IA
+        </p>
+        </section>
         </Layout>
 
     )
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,1 +1,60 @@
+import { useState } from "react"
 import Layout from "../components/Layout"
+import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+import { URLBACKEND } from "../constants";
+
+const AddProduct = () => {
+    const [formData,setFormData] = useState({
+        name:"",
+        description:"",
+        price:"",
+        stock:"",
+        img:"",
+    })
+
+    const navigate = useNavigate();
+    const {token} = useAuth();
+
+     const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const dataToSend = {
+      ...formData,
+      price: Number(formData.price),
+      stock: Number(formData.stock),
+    }
+
+    console.log(token)
+
+    try {
+      const response = await fetch(`${URLBACKEND}/books`, { //ACA CAMBIE EL ENDPOINT
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(dataToSend)
+      })
+
+      if (!response.ok) {
+        alert("❌ Error al cargar el producto")
+        return
+      }
+
+      alert("✅ Éxito al guardar el nuevo producto")
+      setFormData({
+        name: "",
+        description: "",
+        price: "",
+        stock: "",
+        category: "",
+        author:""
+      })
+      navigate("/")
+    } catch (error) {
+
+    }
+  }
+
+}
